e5a25090b654be9908315dc8f364589c1173fdfd
gitter9021001 <69082312+gitter9021001@users.noreply.github.com>
2022-05-12 23:44:46 +0700

    Update guessSquare.js

--- src/component/guessSquare/guessSquare.js
+++ src/component/guessSquare/guessSquare.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import styles from './guessSquareStyle.js'
-
+<!-- Nam's branch -->
 function GuessSquare() {
   return (
 	<View style={styles.guessSquare}></View>
